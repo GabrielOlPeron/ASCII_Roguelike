02db2b1b07af5c967b130c0bcfd022cac379199a
gabriel <gabriel@DESKTOP-RDUB673>
2024-04-04 19:42:41 -0300

    Adicionar arquivos de projeto.

---
+++ ASCII_Roguelike/Program.cs
@@ -0,0 +1,15 @@
+﻿using SadConsole.Configuration;
+
+Settings.WindowTitle = "My SadConsole Game";
+
+
+Builder configuration = new Builder().SetScreenSize(90, 30).UseDefaultConsole().OnStart(Startup);
+
+Game.Create(configuration);
+Game.Instance.Run();
+Game.Instance.Dispose();
+
+static void Startup(object? sender, GameHost host)
+{
+    Console startingConsole = Game.Instance.StartingConsole;
+}
